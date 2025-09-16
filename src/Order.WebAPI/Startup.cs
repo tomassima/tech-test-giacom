b54d0cee2dfcdce163af80e386e2ffb01d722b9d
Tomas Sima <tomassimaofficial@gmail.com>
2025-09-16 23:05:02 +0200

    Configure Swagger for API documentation and adjust connection strings for MySQL

--- src/Order.WebAPI/Startup.cs
+++ src/Order.WebAPI/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Order.Data;
 using Order.Service;
+using System;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore;
 
 namespace OrderService.WebAPI
 {
@@ -33,6 +36,16 @@ namespace OrderService.WebAPI
             services.AddScoped<IOrderRepository, OrderRepository>();
 
             services.AddControllers();
+
+            // Swagger/OpenAPI
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Order API",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,9 +54,22 @@ namespace OrderService.WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API v1"));
+            }
+            else
+            {
+                // Expose Swagger in non-development environments as well if desired
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API v1"));
             }
 
-            app.UseHttpsRedirection();
+            // Avoid forcing HTTPS redirection when running inside a container without TLS configured
+            var runningInContainer = string.Equals(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"), "true", StringComparison.OrdinalIgnoreCase);
+            if (!runningInContainer)
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
